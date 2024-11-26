4be0bd05bbbe212cd3c46a93de44bb520bf27678
Minhoangg <132485081+Minhoangg@users.noreply.github.com>
2024-11-26 21:09:15 +0700

    len code

--- resources/views/client/getscoregame.blade.php
+++ resources/views/client/getscoregame.blade.php
@@ -30,4 +30,23 @@
 
         draw();
     </script>
+
+    <script>
+        // js is for the animation loop for the strokes
+        const textElement = document.getElementById("animatedText");
+
+        function restartAnimation() {
+            textElement.style.transition = "none";
+            textElement.style.strokeDashoffset = "0";
+
+            setTimeout(() => {
+                textElement.style.transition = "stroke-dashoffset 3s ease";
+                textElement.style.strokeDashoffset = "1000";
+            }, 50);
+        }
+
+        // Start de animatie direct en in een loop
+        restartAnimation();
+        setInterval(restartAnimation, 10000);
+    </script>
 @endsection
--- resources/views/livewire/handle-game.blade.php
+++ resources/views/livewire/handle-game.blade.php
@@ -22,16 +22,62 @@
                             <span style="--i:10;"></span>
                         </div>
                     </div>
+
+                    <div id="arrowAnim">
+                        <div class="arrowSliding">
+                            <div class="arrow"></div>
+                        </div>
+                        <div class="arrowSliding delay1">
+                            <div class="arrow"></div>
+                        </div>
+                        <div class="arrowSliding delay2">
+                            <div class="arrow"></div>
+                        </div>
+                        <div class="arrowSliding delay3">
+                            <div class="arrow"></div>
+                        </div>
+                    </div>
+
                 </div>
 
                 <div class="frame_core_bottom d-flex">
                     <div class="frame_core_bottom_left">
-                        <img src="{{ asset('assets/img/ava_1.gif') }}" alt="">
+                        <video autoplay loop muted>
+                            <source src="{{ asset('assets/video/a4dd517c96b44c969b4790deb685a1ff.webm') }}" type="video/mp4">
+                        </video>
                     </div>
                     <div class="frame_core_bottom_right">
                         <div class="frame_core_bottom_right_1">
                             <div class="name">
-                                <h4>win win win win win win</h4>
+                                <svg viewBox="0 0 800 200" xmlns="http://www.w3.org/2000/svg"
+                                    xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    <!-- Định nghĩa pattern cho chữ -->
+                                    <defs>
+                                        <pattern id="imagePattern" patternUnits="userSpaceOnUse" width="400"
+                                            height="400">
+                                            <image href="https://iili.io/2aZNbEJ.png" x="0" y="0" width="400"
+                                                height="400" />
+                                        </pattern>
+
+                                        <!-- Định nghĩa glow filter -->
+                                        <filter id="glowFilter" x="-50%" y="-50%" width="200%" height="200%">
+                                            <feGaussianBlur in="SourceAlpha" stdDeviation="8" result="blurred" />
+                                            <feOffset in="blurred" dx="0" dy="0"
+                                                result="offsetBlurred" />
+                                            <feFlood flood-color="cyan" result="glowColor" />
+                                            <feComposite in="glowColor" in2="offsetBlurred" operator="in" />
+                                            <feMerge>
+                                                <feMergeNode />
+                                                <feMergeNode in="SourceGraphic" />
+                                            </feMerge>
+                                        </filter>
+                                    </defs>
+
+                                    <!-- Text -->
+                                    <text id="animatedText" x="50%" y="50%" dominant-baseline="middle"
+                                        text-anchor="middle">Thánh địa win</text>
+                                </svg>
+
                             </div>
                             <div class="image">
                                 <img src="{{ asset('assets/img/pg.png') }}" alt="" width="80"
@@ -56,7 +102,12 @@
 
                         </div>
                         <div class="frame_core_bottom_right_3">
-                            <button wire:click="handleClick">Nhận Tỉ Lệ</button>
+                            <button class="neon-button">
+                                <span>Nhận Tỉ Lệ</span>
+                                <div class="glow-wrap">
+                                    <div class="glow"></div>
+                                </div>
+                            </button>
                             <img src="{{ asset('assets/img/1_tmp.png') }}" alt="">
                         </div>
                     </div>
